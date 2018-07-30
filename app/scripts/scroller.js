79f017f7360397573d0afb2c50d41f77ff5c62bf
carrot <velvetwall@protonmail.com>
2018-07-30 04:11:40 +0000

    Build once again

--- app/scripts/scroller.js
+++ app/scripts/scroller.js
@@ -28,7 +28,7 @@ module.exports = class Scroller {
                 // Must change focus!
                 var $target = $(target);
                 $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
+                if ($target.is(':focus')) { // Checking if the target was focused
                   return false;
                 } else {
                   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
